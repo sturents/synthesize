da5a97999701e6d6d535d1b4327c927c5ccccf04
Jacob Wyke <jacob@frozensheep.com>
2015-11-12 15:26:29 +0000

    Adding a new enum type.

---
+++ src/Type/Enum.php
@@ -0,0 +1,57 @@
+<?php
+/**
+*	File Containing the Object class.
+*
+*	@package	Frozensheep\Synthesize
+*	@author		Jacob Wyke <jacob@frozensheep.com>
+*	@license	MIT
+*
+*/
+
+namespace Frozensheep\Synthesize\Type;
+
+use Frozensheep\Synthesize\Type\Type;
+use Frozensheep\Synthesize\Exception\ClassException;
+
+/**
+*	Object Class
+*
+*	An object data class.
+*
+*	@package	Frozensheep\Synthesize
+*
+*/
+class Enum extends Type {
+
+	/**
+	*	Setup Method
+	*
+	*	Called after the object is created by the TypeFactory to finish any setup required.
+	*	@return void
+	*/
+	public function setup(){
+
+	}
+
+	/**
+	*	Get Value Method
+	*
+	*	Returns the value for the property.
+	*	@return mixed
+	*/
+	public function &getValue(){
+		$this->mixValue->__toString();
+	}
+
+	/**
+	*	Set Value Method
+	*
+	*	Sets the value for the property.
+	*	@param mixed $mixValue The value to check.
+	*	@throws TypeException if valud is not valid.
+	*	@return bool
+	*/
+	public function setValue($mixValue){
+		$this->mixValue = new {$this->options()->class}($mixValue);
+	}
+}
