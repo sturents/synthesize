2f1071686f37b2badb29413fd6a2feec7c329a67
Craig McMahon <craig.mcmahon@sturents.com>
2025-02-11 13:43:10 +0000

    Add php8.4 compatability

--- src/Type/Type.php
+++ src/Type/Type.php
@@ -49,7 +49,7 @@ class Type implements TypesInterface, \JsonSerializable {
 	*	@param mixed An optional value to set upon construction.
 	*	@return self
 	*/
-	public function __construct($mixValue = null, SynthesizeOption $objOptions = null){
+	public function __construct($mixValue = null, ?SynthesizeOption $objOptions = null){
 		if($mixValue){
 			$this->setValue($mixValue);
 		}
